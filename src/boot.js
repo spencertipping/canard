451a787e8609632ff86b46ac9d74d73063bf2c3d
Spencer Tipping <spencer@spencertipping.com>
2012-05-23 14:48:11 -0600

    Added a repl. The only problem is that it prints [object Object] after each evaluation, but I can probably fix that.

--- src/boot.js
+++ src/boot.js
@@ -1,6 +1,7 @@
 caterwaul.module( 'canard' , (function(e) {var result= (function($) { (function() {var annotate=e.annotate,parsers=e.parsers,bfs=e.bfs,bfc=e.bfc,state_matrix=e.state_matrix,step_matrix_mutable=e.step_matrix_mutable,step_matrix_immutable=e.step_matrix_immutable,row_composite_states_from=e.row_composite_states_from,alt=e.alt,all=e.all,manyc=e.manyc,manyc_one=e.manyc_one,many=e.many,optional=e.optional,step_matrix_immutable_null=e.step_matrix_immutable_null,step_matrix_mutable_null=e.step_matrix_mutable_null,has_non_null_states=e.has_non_null_states,row_null_states_from=e.row_null_states_from,zero=e.zero,fail=e.fail,match=e.match,reject=e.reject,pluralize=e.pluralize,iv=e.iv,map=e.map,flat_map=e.flat_map,map_state=e.map_state,flat_map_state=e.flat_map_state,logical_state=e.logical_state,linear_string_state=e.linear_string_state,anchor_regexp=e.anchor_regexp,linear_string=e.linear_string,linear_regexp=e.linear_regexp,structure_state=e.structure_state,array_state=e.array_state,proxy_state=e.proxy_state,position_state=e.position_state,position=e.position;
 return(function() {var statics=function() {;
-return{syntax:$.merge($.syntax_subclass(tree_ctor() ,tree_methods() ) ,tree_statics() ) ,parse:parser() } } ,parser=function() {;
+return{syntax:$.merge($.syntax_subclass(tree_ctor() ,tree_methods() ) ,tree_statics() ) ,default_bindings:function() {;
+return default_bindings() } ,parse:parser() } } ,parser=function() {;
 return(function() {var toplevel=function(xs) {;
 return toplevel(xs) } ,toplevel=annotate(toplevel, 'toplevel' , [] ) ,term=function(xs) {;
 return term(xs) } ,term=annotate(term, 'term' , [] ) ,maybe_ws=function(p) {;
@@ -78,6 +79,6 @@ xi<xl;
  ++xi)x=xs[xi] ,xr.push( ( [x, {interpret:$.compile($.parse( ( "function (stack) {return {h: caterwaul.canard.syntax.atom(+(+stack.t.h.data " + (x) + " +stack.h.data)), t: stack.t.t}}" ) ) ) } ] ) ) ;
 return xr} ) .call(this, [ '+' , '-' , '*' , '/' , '%' , '>>' , '<<' , '>>>' , '|' , '&' , '^' , '<' , '>' , '<=' , '>=' , '===' , '!==' , '==' , '!=' ] ) ) ) } ;
 return $.merge( ($.canard=function(source,stack,bindings) {;
-return $.canard.parse(source) .interpret(stack||null,$.merge( {} ,default_bindings() ,bindings) ) } ) ,statics() ) } ) .call(this) } ) .call(this) } ) ;
+return $.canard.parse(source) .interpret(stack||null,$.merge( {} ,$.canard.default_bindings() ,bindings) ) } ) ,statics() ) } ) .call(this) } ) .call(this) } ) ;
 result.caterwaul_expression_ref_table= {e: ( "caterwaul.parser" ) } ;
 return(result) } ) .call(this,caterwaul.parser) ) ;
